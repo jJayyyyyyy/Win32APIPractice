75d666929e51c81b9fc921930020d87b1cae2e71
FrozenMap <ljjdbd123@hotmail.com>
2020-02-01 12:33:33 +0000

    add Ellipse.c

---
+++ GUI/1_Shape/2_Filled_Shape/Ellipse.c
@@ -0,0 +1,99 @@
+#include <windows.h>
+
+// gcc Ellipse.c -lgdi32 -mwindows -o Ellipse
+
+/*
+API:
+
+*   [Ellipse](https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-ellipse)
+
+    BOOL Ellipse(
+          [in] HDC hdc,
+          [in] int left,
+          [in] int top,
+          [in] int right,
+          [in] int bottom
+    );
+
+*/
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+    static TCHAR szAppName[] = TEXT("MyWin32App");
+    HWND hwnd;
+    MSG msg;
+    WNDCLASS wndclass;
+
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = szAppName;
+
+    if ( !RegisterClass(&wndclass) )
+    {
+        MessageBox(NULL, TEXT("This program only runs on Windows NT!"), szAppName, MB_ICONERROR);
+        return 0;
+    }
+    
+    hwnd = CreateWindow(szAppName,
+        TEXT("Line"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    while ( GetMessage(&msg, NULL, 0, 0) )
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+void TestEllipse(HDC hdc)
+{
+    HGDIOBJ nullBrush = GetStockObject(NULL_BRUSH);
+    HGDIOBJ originHGDIOBJ = SelectObject(hdc, nullBrush);
+    Ellipse(hdc, 50, 50, 150, 150);
+    Ellipse(hdc, 150, 150, 320, 400);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rect;
+    switch (message)
+    {
+        case WM_PAINT:
+            hdc = BeginPaint(hwnd, &ps);
+            GetClientRect(hwnd, &rect);
+            
+            TestEllipse(hdc);
+
+            EndPaint(hwnd, &ps);
+            return 0;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+}
